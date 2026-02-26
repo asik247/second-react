5f08bd8e4ccbdcc0247a74eaae2e72603a8b0da5
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-26 10:51:05 +0600

    function return  declartion

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,12 +2,17 @@ import React from "react";
 const Nav = ()=>{
     const name = "Asik"
     const number = 10;
+    const greating =()=>{
+        return "Welcome to React"
+    }
     return(
         <>
         {/* variable use */}
         <h1>hi {name}</h1>
         {/* Expression use */}
         <h2>{number + 5}</h2>
+        {/* Function use */}
+        <h3>{greating()}</h3>
 
         </>
     )
