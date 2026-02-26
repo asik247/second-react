36edd554b9a574baa5fe899fa6b4499cb3b39d1e
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-26 10:47:01 +0600

    variable declartion

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,21 +1,14 @@
-// import React from 'react';
-
-// const Nav = () => {
-//     return (
-//         <div>
-            
-//         </div>
-//     );
-// };
-
-// export default Nav;
-
 import React from "react";
 const Nav = ()=>{
+    const name = "Asik"
     return(
         <>
-        <h1>Hi This is my first component in react</h1>
+        {/* <h1>Hi This is my first component in react</h1> */}
+        {/* variable use */}
+        <h1>hi {name}</h1>
+
         </>
     )
 }
 export default Nav
+
