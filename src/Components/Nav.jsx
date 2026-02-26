52266451e41b177f84b462c13fe3281a7b3774da
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-26 10:48:38 +0600

    expression declartion

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 const Nav = ()=>{
     const name = "Asik"
+    const number = 10;
     return(
         <>
-        {/* <h1>Hi This is my first component in react</h1> */}
         {/* variable use */}
         <h1>hi {name}</h1>
+        {/* Expression use */}
+        <h2>{number + 5}</h2>
 
         </>
     )
