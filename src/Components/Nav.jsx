d29749f48fb12e71c9d07b5bbd75d16e0f023539
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-26 10:38:49 +0600

    first components declare

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,21 @@
+// import React from 'react';
+
+// const Nav = () => {
+//     return (
+//         <div>
+            
+//         </div>
+//     );
+// };
+
+// export default Nav;
+
+import React from "react";
+const Nav = ()=>{
+    return(
+        <>
+        <h1>Hi This is my first component in react</h1>
+        </>
+    )
+}
+export default Nav
